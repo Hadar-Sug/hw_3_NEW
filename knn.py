4038bb1722c1ee8a90909a32ff8a8b82e48f288e
Hadar-sug <Hadar.sug@campus.technion.ac.il>
2022-06-09 17:29:05 +0300

    ome comments

--- knn.py
+++ knn.py
@@ -6,12 +6,22 @@ from data import StandardScaler as SS
 
 class KNN:
     def __init__(self, k):
+        """
+        constructor
+        :param k: k used for knn model
+        """
         self.X_train = None
         self.y_train = None
         self.k = k
-        self.ss = SS()
+        self.ss = SS()  # initialize standard scaler object for model
 
     def fit(self, X_train, y_train):
+        """
+        calc mean and std
+        :param X_train: calc stats for this param
+        :param y_train: and this one too
+        :return:
+        """
         self.X_train = X_train
         self.y_train = y_train
         self.ss.fit(X_train)
@@ -21,16 +31,27 @@ class KNN:
         pass
 
     def neighbours_indices(self, x):
+        """
+        find indices of k closest neighbors to x
+        :param x: the point
+        :return: np array of said indices
+        """
         distances = list()
-        X_train = self.ss.transform(self.X_train)
-        for sample in X_train:
+        X_train = self.ss.transform(self.X_train)  # lets transform
+        for sample in X_train:  # get distances of all point from x
             dist = KNN.dist(x, sample)
             distances.append(dist)
-        idx = np.argsort(np.array(distances))
+        idx = np.argsort(np.array(distances))  # get only the first k
         return idx[:self.k]
 
     @staticmethod
     def dist(x1, x2):
+        """
+        euclidian distance method
+        :param x1: np array
+        :param x2: np array
+        :return: euclidian distance between 2 vectors x1 and x2
+        """
         return np.linalg.norm(x1 - x2)
 
 
@@ -61,5 +82,3 @@ class RegressionKNN(KNN):
             sub_list = np.array([self.y_train[idx] for idx in indices])
             pred.append(sub_list.mean())
         return pred
-
-
